e540d26abd5fd6bf8e6930bd66998f21d9979f5d
Saalim398 <saalimkhan1304@gmail.com>
2025-07-20 22:58:26 +0530

    JULY 20 2025

---
+++ dynamicProgramming/largestCommonSubSeq.py
@@ -0,0 +1,12 @@
+def lcs(i, j):
+    if i >= len(a) or j >= len(b):
+        return 0
+    if a[i] == b[j]:
+        return 1 + lcs(i+1, j+1)
+    else:
+        return max(lcs(i+1, j), lcs(i, j+1))
+
+a = "hello"
+b = "shell"
+
+print("Length of LCS:", lcs(0, 0))
